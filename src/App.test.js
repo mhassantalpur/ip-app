87dadd63b228046a9eacf39a803b40f674c9b410
Hassan <hassantalpur22@gmail.com>
2022-05-24 09:55:33 -0700

    (Debugging): implement react testing using jest + testing library

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(() => {
+    cleanup();
+})
+
+test('should render app component', () => {
+    render(<App />);
+    const appElement = screen.getByTestId('app-1');
+    expect(appElement).toBeInTheDocument;
 });
+
+test("should be able to search and display ip address", async () => {
+    render(<App />);
+   //Simulate initiating the search request
+    const searchBtn = screen.getByRole("button", { name: "search" });
+    expect(searchBtn).not.toBeDisabled;
+    userEvent.click(searchBtn);
+    expect(await screen.findByText('Latitude').toBeInTheDocument && await screen.findByText('Longitude').toBeInTheDocument);
+})
